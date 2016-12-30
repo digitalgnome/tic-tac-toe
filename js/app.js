7872694b5088bddd5765c1da5df7e1e0b8cf5828
digitalgnome <digitalgnomes@gmail.com>
2016-12-29 21:39:48 -0800

    Fixed app.js to keep Code button from opeining new tab

--- js/app.js
+++ js/app.js
@@ -379,7 +379,7 @@ $('input[name="reset"]').click(function(event) {
 });
 
 $('input[name="code"]').click(function() {
-  window.open("https://github.com/digitalgnome/tic-tac-toe");
+  window.location.href = "https://github.com/digitalgnome/tic-tac-toe";
 });
 
 })(jQuery);
